965172a70e7626c27691e62d582bfb7918aa325d
fruzi04 <yakerich228@gmail.com>
2024-09-13 13:39:47 +0300

    thunk

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
-import { createRoot } from "react-dom/client";
-import { Provider } from "react-redux";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 
-import App from "./components/app/App";
-import store from "./store";
+import App from './components/app/App';
+import store from './store';
 
-import "./styles/index.scss";
+import './styles/index.scss';
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(
-	<Provider store={store}>
-		<App />
-	</Provider>
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,21 @@
-import { createStore } from 'redux';
-import reducer from '../reducers';
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
+import {thunk} from 'redux-thunk';
+import heroes from '../reducers/heroes';
+import filters from '../reducers/filters';
 
-const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const stringMiddleware = () => (next) => (action) => {
+    if (typeof action === 'string') {
+        return next({
+            type: action
+        })
+    }
+    return next(action)
+};
+
+const store = createStore( 
+                    combineReducers({heroes, filters}),
+                    compose(applyMiddleware(thunk, stringMiddleware),
+                            window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+                    );
 
 export default store;
